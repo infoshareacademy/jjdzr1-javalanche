79e62c722c2cd1c6577d5b5dceca6cd06dea0bc9
Przemek Stepski <przemek.stepski@wp.pl>
2020-06-04 22:22:49 +0200

    initial commit

---
+++ src/main/java/com/infoshareacademy/App.java
@@ -0,0 +1,12 @@
+package com.infoshareacademy;
+
+/**
+ * jAvalanche
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "jAvalanche" );
+    }
+}
