14ba84ef72773a7c548e7013e4248337e4a9059b
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-06-21 18:54:36 +0200

    Empty CRUD methods aded

--- src/main/java/com/infoshareacademy/api/HolidaysDataEditor.java
+++ src/main/java/com/infoshareacademy/api/HolidaysDataEditor.java
@@ -1,4 +1,20 @@
 package com.infoshareacademy.api;
 
 public class HolidaysDataEditor {
+
+    public void createElement(){
+
+    }
+
+    public void readElement(){
+
+    }
+
+    public void updateElement(){
+
+    }
+
+    public void deleteElement(){
+
+    }
 }
