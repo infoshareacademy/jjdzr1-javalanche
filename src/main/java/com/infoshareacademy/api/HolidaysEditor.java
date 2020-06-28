bf00f1be3ca0cfdfcb96f2f0e6b7d4077c3ef804
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-06-28 17:32:57 +0200

    Method updateElement completed.

--- src/main/java/com/infoshareacademy/api/HolidaysEditor.java
+++ src/main/java/com/infoshareacademy/api/HolidaysEditor.java
@@ -1,10 +1,7 @@
 package com.infoshareacademy.api;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -45,6 +42,8 @@ public class HolidaysEditor {
     }
 
     public void createElement() {
+        STDOUT.info("Creating a holiday query. \n ******************** \n\n");
+
         Holidays createHoliday = new Holidays(name(), description(), country(), holidayDate(), type(), locations(), states());
         testList.add(createHoliday);
 
@@ -54,65 +53,96 @@ public class HolidaysEditor {
     }
 
     public void readElement() {
-        STDOUT.info(findHoliday().toString());
+        STDOUT.info("Read a holiday query. \n************************ \n\n");
+
+        STDOUT.info(holidayEdition.get(findHoliday()).toString());
     }
 
     public void updateElement() {
-        testList = new ArrayList<>(holidayEdition);
+        STDOUT.info("Update a holiday query. \n************************ \n\n");
+
+        List<Holidays> updateList = new ArrayList<>(holidayEdition);
 
         foundHolidayIndex = findHoliday();
 
-        Holidays updateHoliday = testList.get(foundHolidayIndex);
+        Holidays updateHoliday = updateList.get(foundHolidayIndex);
 
         System.out.println(updateHoliday.toString());
 
-        STDOUT.info("Enter which element of the holiday's query would you like to change:\n" +
-                "\t1: Holiday's name.\n" +
-                "\t2: Holiday's description\n" +
-                "\t3: Holiday's date\n");
+
 
         Integer usersInput = 0;
 
         do{
+
             isInputInvalid = false;
 
             try {
+                STDOUT.info("Enter which element of the holiday's query would you like to change:\n" +
+                        "1: Holiday's name.\n" +
+                        "2: Holiday's description\n" +
+                        "3: Holiday's date\n");
                 scanner = new Scanner(System.in);
 
                 usersInput = scanner.nextInt();
 
 
                 if(usersInput < 1 || usersInput > 3){
-                    System.out.println("loop response");
+                    STDOUT.error("Input not within required range.\n\n");
                     isInputInvalid = true;
                 }
             }
             catch (Exception e){
-                STDOUT.error("Error found: " + e + "\n" +
-                        "Enter a number between 1 and 3:\n");
+                STDOUT.error("Error found: " + e + "\n" + "Enter a number between 1 and 3:\n");
                 isInputInvalid = true;
             }
+
         }
         while (isInputInvalid);
+
+        System.out.println("loop response 4a");
 
         System.out.println(usersInput);
 
         switch (usersInput) {
             case 1:
                 String updatedHolidaysName = name();
-                updateHoliday.setName(updatedHolidaysName);
+
+                updateList.get(foundHolidayIndex).setName(updatedHolidaysName);
+                //updateHoliday.setName(updatedHolidaysName);
+                break;
             case 2:
                 String updatedHolidaysDescription = description();
                 updateHoliday.setDescription(updatedHolidaysDescription);
+                break;
             case 3:
                 HolidayDate updatedHolidaysDate = holidayDate();
                 updateHoliday.setHolidayDate(updatedHolidaysDate);
-
+                break;
         }
+
+        System.out.println(updateList.get(foundHolidayIndex).toString());
+
     }
 
     public void deleteElement() {
+        STDOUT.info("Delete a holiday query. \n************************ \n\n");
 
+        List<Holidays> deleteTest = new ArrayList<>(holidayEdition);
+
+        foundHolidayIndex = findHoliday();
+
+        System.out.println(foundHolidayIndex + "\n\n");
+
+        deleteTest.remove(foundHolidayIndex);
+
+        int i = 0;
+
+        for (Holidays holiday : deleteTest){
+            System.out.println(i);
+            System.out.println(holiday.toString());
+            i++;
+        }
     }
 
 
@@ -167,6 +197,7 @@ public class HolidaysEditor {
 
     private String name(){
         STDOUT.info("Enter holiday's name\n");
+        scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         STDOUT.info("\n");
         return name;
@@ -174,6 +205,7 @@ public class HolidaysEditor {
 
     private String description(){
         STDOUT.info("Enter holiday's description\n");
+        scanner = new Scanner(System.in);
         String description = scanner.nextLine();
         STDOUT.info("\n");
         return description;
@@ -256,7 +288,7 @@ public class HolidaysEditor {
                 isInputInvalid = true;
             }
 
-            if (requestedMonth < 1 && requestedMonth > 12) isInputInvalid = true;
+            if (requestedMonth < 1 || requestedMonth > 12) isInputInvalid = true;
         }
         while (isInputInvalid);
 
@@ -275,6 +307,8 @@ public class HolidaysEditor {
                 STDOUT.info("Error found:" + e);
                 isInputInvalid = true;
             }
+
+            if (requestedDay < 1 || requestedDay > 31) isInputInvalid = true;
         }
         while (isInputInvalid);
 
