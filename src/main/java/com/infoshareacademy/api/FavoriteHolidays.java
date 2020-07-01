be42ebf4f5926c13ba267cb2be55225aac39d486
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-07-01 18:32:05 +0200

    Methods and fields edited in relation to the class.

--- src/main/java/com/infoshareacademy/api/FavoriteHolidays.java
+++ src/main/java/com/infoshareacademy/api/FavoriteHolidays.java
@@ -2,17 +2,21 @@ package com.infoshareacademy.api;
 
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class FavoriteHolidays {
 
-    private static List<Holidays> favoriteHolidays = HolidaysJsonData.readDataFromJsonFile().getServerResponse().getHolidays();
-    private static Scanner scanner = new Scanner(System.in);
+    private List<Holidays> allHolidaysList = HolidaysJsonData.readDataFromJsonFile().getServerResponse().getHolidays();
+    private List<Holidays> favoriteHolidaysList = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
 
     public void addFavoriteHolidays(){}
 
     public void removeFavoriteHolidays(){}
+
+    public void findFavoriteHoliday(){}
 
     public void printFavoriteHolidays(){}
 
