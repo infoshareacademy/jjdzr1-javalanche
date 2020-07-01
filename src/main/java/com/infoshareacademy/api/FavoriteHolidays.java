4addd2155371eb821f5d6e7aa931c893e7fe678e
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-07-01 18:27:26 +0200

    Basic methods and fields added.

--- src/main/java/com/infoshareacademy/api/FavoriteHolidays.java
+++ src/main/java/com/infoshareacademy/api/FavoriteHolidays.java
@@ -1,4 +1,25 @@
 package com.infoshareacademy.api;
 
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class FavoriteHolidays {
+
+    private static List<Holidays> favoriteHolidays = HolidaysJsonData.readDataFromJsonFile().getServerResponse().getHolidays();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public void addFavoriteHolidays(){}
+
+    public void removeFavoriteHolidays(){}
+
+    public void printFavoriteHolidays(){}
+
+    private void refreshFavoriteHolidays(){}
+
+    private void sortFavoriteHolidays(){}
+
+
+
 }
